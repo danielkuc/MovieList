48be7c14b80239f0507eba2084ce6b53b54e9bed
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-02-12 21:53:37 +0000

    added state, fetch local json, moved local assets to public, rendered json and list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ movie }) => {
   return (
-    <h1>
-      movie #1
-    </h1>
+    <div>
+      <h2>
+        {`#${movie.ranking} - ${movie.title} (${movie.year})`}
+      </h2>
+      <img src={movie.img.src} alt={movie.img.alt} width='200' />
+      <p>{`Distributor: ${movie.distributor}`}</p>
+      <p>{`Amount: ${movie.amount}}`}</p>
+    </div>
   );
 };
 
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
-import Card from '../components/Card'
+import React, { Component } from 'react';
+import Card from '../components/Card';
 
 class List extends Component {
+  state = {
+    data: [],
+    loading: true
+  };
+
+  async componentDidMount() {
+    const response = await fetch('./assets/data.json');
+    const data = await response.json()
+    console.log(data)
+
+    if(data) {
+      this.setState({
+        data,
+        loading: false
+      });
+    }
+  };
+
   render() {
-    return (
-      <Card />
-    )
+    const { data,loading } = this.state;
+
+    if(loading) {
+      return (
+        <div>Loading...</div>
+      );
+    };
+
+    return data.map(movie => <Card key={movie.id} movie={movie} />)
   }
 }
 
